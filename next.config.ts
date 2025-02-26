015c0f209fb4ebdf41df9289918363a212f20401
MD Amirul Islam <islamamirul497@gmail.com>
2025-02-27 02:17:10 +0600

    ok

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV == "production";
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/shadow-actual-portfolio" : "",
+  basePath: isProd ? "/psb-portfolio/" : "",
   output: "export",
   images: { unoptimized: true }
 };
