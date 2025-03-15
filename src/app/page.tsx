1f1bc90416b0913a5d09687705e8b7fc31f1b700
MD Amirul Islam <islamamirul497@gmail.com>
2025-03-16 02:42:47 +0600

    dd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Navbar />
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Vushki</h1>
+        <h1 className="text-4xl font-bold mb-4">Welcome to PSB</h1>
         <p className="text-lg mb-8">Your go-to destination for cutting-edge technology solutions.</p>
         <Link href="/service" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Explore Our Services
